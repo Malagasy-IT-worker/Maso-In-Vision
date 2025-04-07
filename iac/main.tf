af845084a84363b6557e397b58e8b08891638ed6
ankoay <ankoayfeno@gmail.com>
2025-04-07 19:08:39 +0000

    adding resource

--- iac/main.tf
+++ iac/main.tf
@@ -1,3 +1,10 @@
+module "storage" {
+    source = "./Modules/storage"
+    storage_name = "ankoay-storage"
+    storage_location = "africa-south1"
+}
 
-
+module "MIV-SERVER" {
+  source = "./Environments/MIV-SERVER"
+}
 
