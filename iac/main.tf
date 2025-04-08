59f0a92aaff1f1ccc1f0882fe5806af78b3400b5
ankoay feno <ankoayfeno@gmail.com>
2025-04-08 11:09:26 +0300

    infra creation

--- iac/main.tf
+++ iac/main.tf
@@ -1,10 +1,9 @@
 module "storage" {
     source = "./Modules/storage"
-    storage_name = "ankoay-storage"
+    storage_name = "miv-storage"
     storage_location = "africa-south1"
 }
 
 module "MIV-SERVER" {
   source = "./Environments/MIV-SERVER"
 }
-
